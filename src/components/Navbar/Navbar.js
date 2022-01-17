a138ebaced1376211470693f8386fd8f853f4e0c
HungNguyen1213 <52905968+HungNguyen1213@users.noreply.github.com>
2022-01-17 11:06:12 +0700

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+
+import logo from "../../assets/logo.png";
+import useStyles from "./styles";
+
+function Navbar({ totalItems }) {
+  const classes = useStyles();
+  return (
+    <div>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            <img
+              src={logo}
+              alt="Commerce.js"
+              height="25px"
+              className={classes.image}
+            />
+            Catena Shop
+          </Typography>
+          <div className={classes.grow}></div>
+          <div className={classes.butotn}>
+            <IconButton aria-label="Show cart item" color="inherit">
+              <Badge badgeContent={totalItems} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ src/lib/commerce.js
@@ -0,0 +1,4 @@
+import Commerce from "@chec/commerce.js";
+export const commerce = new Commerce(
+  "pk_38563f0ab1bc68e762935f30a63557f5a2a223a556160"
+);
