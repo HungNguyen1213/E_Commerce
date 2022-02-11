24fdebb9d2dee572df0d15239c34d55ef47060bb
HungNguyen1213 <52905968+HungNguyen1213@users.noreply.github.com>
2022-02-11 16:17:12 +0700

    commit test

--- src/components/CheckoutForm/PaymentForm.js
+++ src/components/CheckoutForm/PaymentForm.js
@@ -1,8 +1,20 @@
 import React from "react";
 import { Typography, Button, Divider } from "@material-ui/core";
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  Elements,
+  CardElement,
+  ElementsConsumer,
+} from "@stripe/react-stripe-js";
 
-function PaymentForm() {
-  return <div>Payment Form</div>;
+import Review from "./Review";
+
+function PaymentForm({ checkoutToken }) {
+  return (
+    <>
+      <Review checkoutToken={checkoutToken} />
+    </>
+  );
 }
 
 export default PaymentForm;
